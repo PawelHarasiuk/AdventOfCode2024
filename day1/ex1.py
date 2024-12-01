aaf8d3f4e3691cccc8c469f92ef0c3e954f09b08
PawelHarasiuk <pharasiuk01@gmail.com>
2024-12-01 19:17:13 +0100

    day1: finished ex1

---
+++ day1/ex1.py
@@ -0,0 +1,27 @@
+def read_file():
+    with open("ex1_input.txt", 'r') as f:
+        lines = f.readlines()
+
+    return lines
+
+def split_to_lists(lines):
+    input_list1, input_list2 = [], []
+
+    for line in lines:
+        split_line = line.strip().split("   ")
+        input_list1.append(int(split_line[0]))
+        input_list2.append(int(split_line[1]))
+
+    return input_list1, input_list2
+
+def calculate_distances(input_list1, input_list2):
+    l1 = sorted(input_list1)
+    l2 = sorted(input_list2)
+    total = sum([abs(x1 - x2) for x1, x2 in zip(l1, l2)])
+
+    return total
+
+
+lines = read_file()
+input_list1, input_list2 = split_to_lists(lines)
+print(calculate_distances(input_list1, input_list2))
